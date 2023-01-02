da1806751dccaddbe4adbc9e2a425df457a4fcd4
PeaceOloruntoba <profprincepeace@gmail.com>
2023-01-02 21:00:01 +0100

    progress commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,135 @@
+import React, {useState, useEffect} from 'react'
+import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai'
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { useRouter } from 'next/router'
+
+const Navbar = () => {
+    const[nav, setNav] = useState(false)
+    const [shadow, setShadow] = useState(false)
+    const [navBg, setNavBg] = useState('#ecf0f3')
+    const [linkColor, setLinkColor] = useState('#1f2937')
+    const [position, setPosition] = useState('fixed')
+    const router = useRouter()
+    useEffect (()=>{
+        if(
+            router.asPath === '/property' ||
+            router.asPath === '/crypto' ||
+            router.asPath === '/netflix' ||
+            router.asPath === '/twitch' 
+        ){
+            setNavBg('transparent')
+            setLinkColor('#ecf0f3')
+        }else {
+            setNavBg('#ecf0f3')
+            setLinkColor('#1f2937')
+        }
+    },[router])
+    const handleNav = () => {
+        setNav(!nav)
+    }
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90){
+                setShadow(true)
+            } else {
+                setShadow(false)
+            }
+        }
+        window.addEventListener('scroll', handleShadow)
+    },[])
+  return (
+    <div style={{bacckgroundColor:`${navBg}`}} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+            <a href='/'>
+                <img src='/../public/assets/navLogo.png' alt='/' width='125' height='50' />
+            </a>
+            <div>
+                <ul style={{color: `${linkColor}`}} className='hidden md:flex'>
+                    <a href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                    </a>
+                    <a href='/#about'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                    </a>
+                    <a href='/#skills'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                    </a>
+                    <a href='/#projects'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                    </a>
+                    <a href='/#contact'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                    </a>
+                </ul>
+                <div style={{color: `${linkColor}`}} onClick={handleNav} className='md:hidden'>
+                    <AiOutlineMenu size={25} />
+                </div>
+            </div>
+        </div>
+        <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+            <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+                <div>
+                    <div className='flex w-full items-center justify-between'>
+                        <a href='/'>
+                            <img src='/../public/assets/navLogo.png' width='87' height='35' alt='/' />
+                        </a>
+                        <div onClick={handleNav} className='roundes-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'>
+                        <p className='w-[85%] md:w-[90%] py-4'>Lets build something legendary together</p>
+                    </div>
+                </div>
+                <div className='py-4 flex flex-col'>
+                    <ul className='uppercase'>
+                        <a href='/'>
+                            <li onClick={()=> setNav(false)} className='py-4 text-sm'>Home</li>
+                        </a>
+                        <a href='/#about'>
+                            <li onClick={()=> setNav(false)} className='py-4 text-sm'>About</li>
+                        </a>
+                        <a href='/#skills'>
+                            <li onClick={()=> setNav(false)} className='py-4 text-sm'>Skills</li>
+                        </a>
+                        <a href='/#projects'>
+                            <li onClick={()=> setNav(false)} className='py-4 text-sm'>Projects</li>
+                        </a>
+                        <a href='/#contact'>
+                            <li onClick={()=> setNav(false)} className='py-4 text-sm'>Contact</li>
+                        </a>
+                    </ul>
+                    <div className='pt-40'>
+                        <p className='uppercase tracking-widest text-[#5651e5]'>Let&#39;s Connect</p>
+                        <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                            <a href='' target='_blank' rel='noreferrer'>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaLinkedinIn />
+                                </div>
+                            </a>
+                            <a href='' target='_blank' rel='noreferrer'>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaGithub />
+                                </div>
+                            </a>
+                            <a href='#/contact'>
+                                <div onClick={() => setNav(!nav)} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <AiOutlineMail />
+                                </div>
+                            </a>
+                            <a href='/resume' target='_blank' rel='noreferrer'>
+                                <div onClick={() => setNav(!nav)} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,192 @@
+import React from 'react'
+import Html from '../../public/assets/skills/html.png';
+import Css from '../../public/assets/skills/css.png';
+import Javascript from '../../public/assets/skills/javascript.png';
+import ReactImg from '../../public/assets/skills/react.png';
+import Tailwind from '../../public/assets/skills/tailwind.png';
+import Github from '../../public/assets/skills/github1.png';
+import Firebase from '../../public/assets/skills/firebase.png';
+import NextJS from '../../public/assets/skills/nextjs.png'
+import AWS from '../../public/assets/skills/aws.png';
+import Django from '../../public/assets/skills/django.png'
+import Python from '../../public/assets/skills/python.png'
+import Laravel from '../../public/assets/skills/laravel.png'
+import Figma from '../../public/assets/skills/figma.png'
+import Node from '../../public/assets/skills/node.png'
+import Mongo from '../../public/assets/skills/mongo.png'
+
+const Skills = () => {
+  return (
+    <div id='skills' className='w-full lg:h-screen p-2'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Skills
+        </p>
+        <h2 className='py-4'>What I Can Do</h2>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Html} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Css} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Javascript} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={ReactImg} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Tailwind} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Tailwind</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Django} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Django</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Laravel} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Laravel</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Python} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Python</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Figma} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Figma</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Firebase} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Firebase</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={NextJS} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>NextJs</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Github} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>GitHub</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={AWS} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Amazon Web Services</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Github} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Solidity</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Node} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>NodeJS</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <img src={Mongo} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>MongoDB</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Skills;
